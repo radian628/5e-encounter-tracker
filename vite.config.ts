bafe435394b966682b47c97b6338879aad0b4214
radian628 <70352880+radian628@users.noreply.github.com>
2022-09-17 23:29:14 -0700

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from "rollup-plugin-polyfill-node";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 //import * as util from "util";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills({
-    include: ["assert"]
+    include: ["assert", "process"]
   })],
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodePolyfills({
+          include: ["assert", "process"]
+        })
+      ]
+    }
+  },
   base: "./",
   define: {
-    process: {
-      env: {}
+    global: '({})',
+    // process: {
+    //   env: {},
+    //   stdout: {
+    //     write: console.log
+    //   }
+    // }
+  },
+  resolve: {
+    alias: {
+      process: "rollup-plugin-polyfill-node"
     }
-  }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis"
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
 })
